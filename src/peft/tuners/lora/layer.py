ced144251a86c5f52b4a86bc4306bd02b3fa9ddd
Vishal Bakshi <vdbakshi@gmail.com>
2025-03-18 21:55:18 -0700

    Print cast_to_fp32 value

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -679,6 +679,7 @@ class Linear(nn.Module, LoraLayer):
         # (b)float16 while being on CPU, we need to cast the weights to float32, perform the merge and then cast back to
         # (b)float16 because some CPUs have slow bf16/fp16 matmuls.
         cast_to_fp32 = device.type == "cpu" and (dtype == torch.float16 or dtype == torch.bfloat16)
+        print(f"cast_to_fp32: {cast_to_fp32}") # added by Vishal
 
         weight_A = self.lora_A[adapter].weight
         weight_B = self.lora_B[adapter].weight
