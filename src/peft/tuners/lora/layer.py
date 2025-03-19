6a98acc85f2a792b946bc739a65b941cc8eb1970
Vishal Bakshi <vdbakshi@gmail.com>
2025-03-18 23:05:54 -0700

    Update layer.py

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -573,7 +573,6 @@ class Linear(nn.Module, LoraLayer):
         for active_adapter in adapter_names:
             if active_adapter in self.lora_A.keys():
                 base_layer = self.get_base_layer()
-                print(f"base_layer.dtype: {base_layer.dtype}") # added by Vishal
                 og_base_layer_weight = deepcopy(base_layer.weight) # added by Vishal
                 if safe_merge:
                     # Note that safe_merge will be slower than the normal merge
